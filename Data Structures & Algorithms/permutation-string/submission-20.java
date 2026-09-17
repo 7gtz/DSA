7e3926419edb2d301591b8b7ef9b9589f53a4f46
7gtz <71064991+7gtz@users.noreply.github.com>
2026-09-17 19:27:26 +0530

    Add: permutation-string - submission-20

---
+++ Data Structures & Algorithms/permutation-string/submission-20.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1char = new int[26];
+        int[] window = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1char[s1.charAt(i) - 'a']++;
+            window[s2.charAt(i) - 'a']++;
+
+        }
+
+        if (Arrays.equals(s1char,window)) {
+            return true;
+        }
+
+        int left = 1, right = s1.length();
+
+        while (right < s2.length()) {
+            window[s2.charAt(left-1) - 'a']--;
+            window[s2.charAt(right) - 'a']++;
+
+            if (Arrays.equals(s1char,window)) {
+            return true;
+            }
+
+            left++;
+            right++;
+        }
+
+        return false;
+    }
+}
